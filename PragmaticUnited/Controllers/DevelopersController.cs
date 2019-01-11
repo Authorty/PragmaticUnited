6e8da4d47e990a3205401585c1df405e20571558
Authorty <itismejody@GMAIL.COM>
2019-01-10 22:06:50 -0600

    Add project files.

---
+++ PragmaticUnited/Controllers/DevelopersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PragmaticUnited.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        // GET: Developers
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult JodyBartelt()
+        {
+            return View();
+        }
+        public ActionResult JoshWegerer()
+        {
+            return View();
+        }
+        public ActionResult KevinCraft()
+        {
+            return View();
+        }
+        public ActionResult ChrisCiske()
+        {
+            return View();
+        }
+    }
+}
